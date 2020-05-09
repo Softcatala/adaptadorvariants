e9d035cfed6b112ad995423b93137b4a54ccbb9c
Xavi Ivars <xivarsribes@vistaprint.com>
2020-05-09 19:48:24 +0200

    Fix virtualbox

--- tools/virtualbox/virtualbox-valencia.sh
+++ tools/virtualbox/virtualbox-valencia.sh
@@ -3,7 +3,10 @@
 wget https://www.virtualbox.org/export/HEAD/vbox/trunk/src/VBox/Frontends/VirtualBox/nls/VirtualBox_ca.ts
 wget https://www.virtualbox.org/export/HEAD/vbox/trunk/src/VBox/Frontends/VirtualBox/nls/qt_ca.ts
 
-docker build . -t qt
+docker build -t qt --build-arg SED="$(cat ../src2valencia.sed)"  .
+
 docker create --name virtualbox_valencia -ti qt
+
 docker cp virtualbox_valencia:/data/*valencia.ts .
+
 docker prune virtualbox_valencia
