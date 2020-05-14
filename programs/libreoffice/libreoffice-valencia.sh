9fdcf81ef476dc7a264f56044b0f769c96e8cc10
Joan Montané <joan@montane.cat>
2020-05-14 11:51:31 +0200

    LibreOffice: adapto dades traductor i equip

--- programs/libreoffice/libreoffice-valencia.sh
+++ programs/libreoffice/libreoffice-valencia.sh
@@ -2,13 +2,25 @@
 #/bin/bash
 
 wd="programs/libreoffice"
+translator="Artur V. <artur@softcatala.org>"
+team="Catalan (Valencian) <adaptacions@softvalencia.org>"
 
+# Ens movem al directori de treball
 cd $wd
+
+# Copiem els scripts d'adaptació
+cp -R ../../tools .
+
+# Els adaptem amb les dades del responsable i de l'equip d'adaptacions
+#s/^\(\"Last-Translator\): .*\\n/\1: Jo Mateix <jo@jo.cat>\\n/g
+sed -i -e "s|^:END$|s\/^\\\(\\\\\"Last-Translator\\\): .*\\\\\\\n/\\\\1: $translator\\\\\\\n/g\\n\\n:END|" ./tools/src2valencia.sed
+sed -i -e "s|^:END$|s\/^\\\(\\\\\"Language-Team\\\): .*\\\\\\\n/\\\\1: $team\\\\\\\n/g\\n\\n:END|" ./tools/src2valencia.sed
 
 if [ -d "translations/.git" ]; then
  # Actualitzem les traduccions des d'upstream
- git pull origin master
+ git reset --hard HEAD
  git checkout master
+ git pull origin master
 else
   # Baixem les traduccions des d'upstream
   git clone "https://git.libreoffice.org/translations"
@@ -18,7 +30,7 @@ fi
 rm -R "translations/source/ca-valencia"
 
 # Tornem a generar els fitxers de ca-valencia
-../../tools/adapta_arbre ./translations/source/ca ../../tools/src2valencia.sed
+./tools/adapta_arbre ./translations/source/ca ./tools/src2valencia.sed
   
 # Comprovacions mínimes?
 
@@ -30,6 +42,8 @@ rm -R "translations/source/ca-valencia"
 # git push
 # cd ..
 
+# Neteja
+rm -R tools
 # Tornem al directori inicial
 cd ../../
 
