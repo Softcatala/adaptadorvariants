2547f658f350b038f31bcbbefa0fa84b69c6e164
Joan Montané <joan@montane.cat>
2020-05-23 23:04:58 +0200

    LibreOffice using Weblate API

--- programs/libreoffice/libreoffice-valencia.sh
+++ programs/libreoffice/libreoffice-valencia.sh
@@ -15,33 +15,15 @@ cp -R ../../tools .
 sed -i -e "s|^:END$|s\/^\\\(\\\\\"Last-Translator\\\): .*\\\\\\\n/\\\\1: $translator\\\\\\\n/g\\n\\n:END|" ./tools/src2valencia.sed
 sed -i -e "s|^:END$|s\/^\\\(\\\\\"Language-Team\\\): .*\\\\\\\n/\\\\1: $team\\\\\\\n/g\\n\\n:END|" ./tools/src2valencia.sed
 
-if [ -d "translations/.git" ]; then
- # Actualitzem les traduccions des d'upstream
- git reset --hard HEAD
- git checkout master
- git pull origin master
-else
-  # Baixem les traduccions des d'upstream
-  git clone "https://git.libreoffice.org/translations"
-fi
+# Neteja general
+rm -R "translations/*"
 
-# Esborrem tots els fitxers de ca-valencia
-rm -R "translations/source/ca-valencia"
+# Cridem l'script de Python que fa la feina
+./libreoffice-valencia.py
 
-# Tornem a generar els fitxers de ca-valencia
-./tools/adapta_arbre ./translations/source/ca ./tools/src2valencia.sed
-  
-# Comprovacions mínimes?
-
-# Si tot sembla correcte, enviem els canvis
-# cd translations
-# git add source/ca-valencia/*
-# git commit -m "Update Catalan (Valencian) ca-valencia files"
-# git pull origin master
-# ./logerrit submit master
-# cd ..
 
 # Neteja
 rm -R tools
+
 # Tornem al directori inicial
 popd
