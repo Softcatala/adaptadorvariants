d26da7da565f93f489d504e8555863fb73cf8f14
Jaume Ortolà <jaumeortola@gmail.com>
2020-10-04 22:36:40 +0200

    Update adapta.sh

--- adaptador-languagetool/adapta.sh
+++ adaptador-languagetool/adapta.sh
@@ -8,8 +8,5 @@ lt_dir=~/target-lt
 
 enabledRules="EXIGEIX_ACCENTUACIO_VALENCIANA,EXIGEIX_VERBS_VALENCIANS,EXIGEIX_POSSESSIUS_U,PARTICIPIS_IT,ADAPTADOR_VALENCIA_LEXIC,HUI,LEXIC_VAL,PREFERENCIES_VERBS_VALENCIANS,EVITA_DEMOSTRATIUS_AQUEIX"
 
-java -jar $lt_dir/languagetool-commandline.jar -l ca-ES-valencia -e $enabledRules -eo -a \
-| sed "s/de eixir/d'eixir/"
-
-# 2> /dev/null
+java -jar $lt_dir/languagetool-commandline.jar -l ca-ES-valencia -e $enabledRules -eo -a #2> /dev/null
 
