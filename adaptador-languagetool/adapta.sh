18ff8901fbaa7f5974937219fa3d38b16e998ec3
jaumeortola <jaumeortola@gmail.com>
2020-10-04 14:03:10 +0200

    adaptador amb Languagetool

---
+++ adaptador-languagetool/adapta.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#Use
+#./adapta.sh < test.txt
+
+#LanguageTool directory
+lt_dir=~/target-lt
+
+enabledRules="EXIGEIX_ACCENTUACIO_VALENCIANA,EXIGEIX_VERBS_VALENCIANS,EXIGEIX_POSSESSIUS_U,PARTICIPIS_IT,ADAPTADOR_VALENCIA_LEXIC,HUI,LEXIC_VAL,PREFERENCIES_VERBS_VALENCIANS,EVITA_DEMOSTRATIUS_AQUEIX"
+
+java -jar $lt_dir/languagetool-commandline.jar -l ca-ES-valencia -e $enabledRules -eo -a \
+| sed "s/de eixir/d'eixir/"
+
+# 2> /dev/null
+
